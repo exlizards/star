c2ceea1c72f7ccfe731c4e7fae34d39f32e26d45
exlizards <40582972+exlizards@users.noreply.github.com>
2019-05-25 00:35:21 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php 
+
+ $uuid = "55cc7780-881e-44e3-867d-9b0b2f85fef6";
+
+ $carrier = "XL";
+
+ //Google-Advertiser-Id
+ $idd = "112a1ab9-25e4-4e6f-abfd-821d2de5f108";
+
+ //Device-Name
+ $device = "Redmi Note 5";
+
+ //Android-Id
+ $android = "5a22e91452aeeb7f";
+ 
+ //Authorization
+ $auth = "Bearer 2cd79921-368b-4731-8ae6-27846b1b79af";
+ 
+?>
+
---
+++ un.php
@@ -0,0 +1,379 @@
+/* Decoded by unphp.net */
+
+<?php $ {
+    "GLOBALS"
+}
+["ynuhebeqfswc"] = "u";
+$ {
+    "GLOBALS"
+}
+["tjwzftkeyj"] = "t";
+$ {
+    "GLOBALS"
+}
+["sizxshjtdu"] = "p";
+$ {
+    "GLOBALS"
+}
+["pucouljvu"] = "js";
+$ {
+    "GLOBALS"
+}
+["sfxiiiqk"] = "uuid";
+$ {
+    "GLOBALS"
+}
+["dhfpvvdl"] = "head";
+$ {
+    "GLOBALS"
+}
+["vjnipkokppvo"] = "ch";
+$ {
+    "GLOBALS"
+}
+["mhdlstsf"] = "be";
+$ {
+    "GLOBALS"
+}
+["rqslxx"] = "date";
+date_default_timezone_set("");
+@system("clear");
+
+error_reporting(0);
+require ("config.php");
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["rqslxx"]
+} = new DateTime();
+echo $date->format("H:i:s d/m/Y") . "
+";
+
+print "
+[32m
+--------------------[32m
+Tool app Star Gift[32m
+Created By: Bi Silk[32m
+Youtube: Bi Silk[32m
+[ Like & Subscribe ]
+--------------------[0m
+	";
+echo "
+";
+
+while (true) {
+    $qokoibeq = "ch";
+    $ubfxjjxstygu = "rees";
+    $ {
+        "GLOBALS"
+    }
+    ["wdihntc"] = "b";
+    $qnyycrrmwa = "ch";
+    $jqyqjopt = "rees";
+    $fjllhvgrptoe = "head";
+    $ {
+        "GLOBALS"
+    }
+    ["wfspepodutx"] = "ch";
+    $kxcrqcvrmuh = "head";
+    $ {
+        "GLOBALS"
+    }
+    ["yzfrluimv"] = "ch";
+    $ {
+        "GLOBALS"
+    }
+    ["lemltknftqbo"] = "res";
+    $bjdogvynq = "js";
+    $ {
+        "GLOBALS"
+    }
+    ["zwefyikbixxx"] = "bo";
+    $eyvvaslyxj = "be";
+    $ {
+        "GLOBALS"
+    }
+    ["ibqgmyxxn"] = "js";
+    $lpbxiftnt = "ch";
+    $ {
+        $fjllhvgrptoe
+    } = array("App-Version: 1.0.0", "Build-Num: 12", "Country: VN", "Connection-Type: NETWORK_TYPE_WIFI", "Carrier: $carrier", "OS-Version: 25", "Google-Advertiser-Id: $idd", "Device-Name: $device", "Device-Type: 2", "Timezone: Asia/Saigon", "Android-Id: $android", "Authorization: $auth", "Content-Type: application/json; charset=UTF-8", "Host: api.stargift.co", "Connection: Keep-Alive", "Accept-Encoding: gzip", "User-Agent: okhttp/3.10.0");
+    $ {
+        "GLOBALS"
+    }
+    ["krofsf"] = "ch";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["wdihntc"]
+    } = array("data" => array(),);
+    $jvotykw = "ch";
+    $uorknhj = "ch";
+    $sqhhwfleotk = "ch";
+    $ppitgno = "ch";
+    $gasclvgoamc = "ress";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["zwefyikbixxx"]
+    } = json_encode($ {
+        $eyvvaslyxj
+    });
+    $ {
+        "GLOBALS"
+    }
+    ["jwiwxrke"] = "res";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["mhdlstsf"]
+    } = array("uuid" => "$uuid");
+    $ {
+        $jvotykw
+    } = curl_init();
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    }, CURLOPT_URL, "http://api.stargift.co/giphy/collect");
+    $ {
+        "GLOBALS"
+    }
+    ["ggyypiyokbki"] = "ch";
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["krofsf"]
+    }, CURLOPT_RETURNTRANSFER, true);
+    $acqjzketojmy = "ch";
+    $ {
+        "GLOBALS"
+    }
+    ["cuiaqeacs"] = "ch";
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    }, CURLOPT_SSL_VERIFYPEER, 1);
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    }, CURLOPT_HTTPHEADER, $ {
+        $ {
+            "GLOBALS"
+        }
+        ["dhfpvvdl"]
+    });
+    curl_setopt($ {
+        $qnyycrrmwa
+    }, CURLOPT_CUSTOMREQUEST, "POST");
+    $ {
+        "GLOBALS"
+    }
+    ["tedfbvdo"] = "uuid";
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["wfspepodutx"]
+    }, CURLOPT_POSTFIELDS, "{
+	\"uuid\":\"" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["sfxiiiqk"]
+    } . "\"
+}");
+    $ {
+        $gasclvgoamc
+    } = curl_exec($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    });
+    curl_close($ {
+        $sqhhwfleotk
+    });
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    } = curl_init();
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    }, CURLOPT_URL, "http://api.stargift.co:80/lockscreen/bonus");
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["cuiaqeacs"]
+    }, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ {
+        $qokoibeq
+    }, CURLOPT_SSL_VERIFYPEER, 1);
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    }, CURLOPT_HTTPHEADER, $ {
+        $kxcrqcvrmuh
+    });
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["ggyypiyokbki"]
+    }, CURLOPT_CUSTOMREQUEST, "GET");
+    $ {
+        $jqyqjopt
+    } = curl_exec($ {
+        $acqjzketojmy
+    });
+    $pdhckt = "js";
+    $cdquboptwxki = "ch";
+    curl_close($ {
+        $ppitgno
+    });
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["pucouljvu"]
+    } = json_decode($ {
+        $ubfxjjxstygu
+    }, true);
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["sizxshjtdu"]
+    } = $ {
+        $bjdogvynq
+    }
+    ["next"]["timeLeftSeconds"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    } = curl_init();
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["yzfrluimv"]
+    }, CURLOPT_URL, "http://api.stargift.co/user/" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tedfbvdo"]
+    } . "/points");
+    $ {
+        "GLOBALS"
+    }
+    ["frlvmtchjps"] = "err";
+    $ {
+        "GLOBALS"
+    }
+    ["pcegkok"] = "u";
+    curl_setopt($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    }, CURLOPT_RETURNTRANSFER, true);
+    $dpouhsijkc = "head";
+    curl_setopt($ {
+        $cdquboptwxki
+    }, CURLOPT_SSL_VERIFYPEER, 1);
+    curl_setopt($ {
+        $lpbxiftnt
+    }, CURLOPT_HTTPHEADER, $ {
+        $dpouhsijkc
+    });
+    curl_setopt($ {
+        $uorknhj
+    }, CURLOPT_CUSTOMREQUEST, "GET");
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["lemltknftqbo"]
+    } = curl_exec($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    });
+    curl_close($ {
+        $ {
+            "GLOBALS"
+        }
+        ["vjnipkokppvo"]
+    });
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["pucouljvu"]
+    } = json_decode($ {
+        $ {
+            "GLOBALS"
+        }
+        ["jwiwxrke"]
+    }, true);
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["pcegkok"]
+    } = $ {
+        $pdhckt
+    }
+    ["bank"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tjwzftkeyj"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["ibqgmyxxn"]
+    }
+    ["timetoplay"];
+    if ($ {
+        $ {
+            "GLOBALS"
+        }
+        ["frlvmtchjps"]
+    } == "Ok") {
+        echo "Error";
+    } else {
+        echo "- Balance : " . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["ynuhebeqfswc"]
+        } . "
+";
+
+    }
+}
+?>
